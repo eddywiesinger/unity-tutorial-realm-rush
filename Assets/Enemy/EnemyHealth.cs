546d8e0e52016ad3fb39be00c106041562a6c8f2
Eddy Wi <wiesinger.edwin@gmail.com>
2023-06-30 16:06:44 +0200

    add currency system

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -6,10 +6,16 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
     [SerializeField] int currentHitPoints = 0;
-    // Start is called before the first frame update
+
+    Enemy enemy;
     void OnEnable()
     {
         currentHitPoints = maxHitPoints;
+    }
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
     }
 
     void OnParticleCollision(GameObject other)
@@ -24,6 +30,7 @@ public class EnemyHealth : MonoBehaviour
         if (currentHitPoints <= 0)
         {
             gameObject.SetActive(false);
+            enemy.RewardGold();
         }
     }
 }
--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -1,24 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
-
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     void Update()
     {
-        AimWeapon();    
+        FindClosestTarget();
+        AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
         weapon.LookAt(target);
+        if (targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        EmissionModule emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
