23022d83969609ea1da5eb207f7eeb1b35932c86
eddywiesinger <wiesinger.edwin@gmail.com>
2023-07-04 15:54:53 +0200

    add FX to enemy hit

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 [RequireComponent(typeof(Enemy))] 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
+    [SerializeField] AudioSource audioSource;
 
     [Tooltip("Adds amount to maxHitPoints when enemy dies.")]
     [SerializeField] int difficultyRamp = 1;
     
     int currentHitPoints = 0;
+    ParticleSystem hitParticles;
 
     Enemy enemy;
     void OnEnable()
     {
         currentHitPoints = maxHitPoints;
+        hitParticles = GetComponentInChildren<ParticleSystem>();
     }
 
     void Start()
     {
         enemy = GetComponent<Enemy>();
+        audioSource = GameObject.Find("AudioManager").GetComponent<AudioSource>();
     }
 
     void OnParticleCollision(GameObject other)
@@ -31,6 +36,8 @@ public class EnemyHealth : MonoBehaviour
     void ProcessHit()
     {
         currentHitPoints--;
+        audioSource.Play();
+        hitParticles.Play();
 
         if (currentHitPoints <= 0)
         {
